cc4367e951163c5519c859d816ac6700db5788d7
Chris <chris_oleary@hotmail.co.uk>
2020-12-08 09:04:22 +0000

    db updates

--- EverNoteCloneWpf/ViewModel/Helpers/DatabaseHelper.cs
+++ EverNoteCloneWpf/ViewModel/Helpers/DatabaseHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -8,5 +9,18 @@ namespace EverNoteCloneWpf.ViewModel.Helpers
     public class DatabaseHelper
     {
         private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
+
+        public static bool Insert<T>(T item)
+        {
+            bool result = false;
+
+            using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+            {
+                conn.CreateTable<T>();
+            }
+
+
+            return result;
+        }
     }
 }
